8ac630503ad4486c56957597d513b7746c155ac8
Jannes Höke <mail@jhoeke.de>
2020-05-25 14:55:11 +0200

    ✨ Allow CORS

--- twig/handler.py
+++ twig/handler.py
@@ -5,5 +5,12 @@ from twig import twitter
 
 def status(event, context):
     id = event["pathParameters"]["id"]
-    response = {"statusCode": 200, "body": json.dumps(twitter.status(id))}
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True,
+        },
+        "body": json.dumps(twitter.status(id)),
+    }
     return response
